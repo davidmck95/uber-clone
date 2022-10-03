936573aebb6e8055d336ac86d709f8b100f841ef
Dee McKinney <deeshtc@gmail.com>
2022-10-03 14:25:43 +0200

    working with directions api - not zooming out to show full route

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,46 +12,46 @@ const HomeScreen = () => {
 
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
-        <View style={tw`p-5`}>
-            <Image 
-                style={{
-                    width: 100,
-                    height: 100,
-                    resizeMode: 'contain'
-                }}
-                source={{
-                    uri: 'https://links.papareact.com/gzs'
-                }}
-            />
-            <GooglePlacesAutocomplete
-                placeholder='Where from?'
-                nearbyPlacesAPI='GooglePlacesSearch'
-                debounce={400}
-                query={{
-                    key: GOOGLE_MAPS_API_KEY,
-                    language: 'en'
-                }}
-                fetchDetails={true}
-                onPress={(data, details = null) => {
-                    dispatch(setOrigin({
-                        location: details?.geometry?.location,
-                        description: data?.description
-                    }))
-                    dispatch(setDestination(null));
-                }}
-                enablePoweredByContainer={false}
-                minLength={2}
-                styles={{
-                    container: {
-                        flex: 0
-                    },
-                    textInput: {
-                        fontSize: 18
-                    }
-                }}
-            />
-            <NavOptions/>
-        </View>
+            <View style={tw`p-5`}>
+                <Image 
+                    style={{
+                        width: 100,
+                        height: 100,
+                        resizeMode: 'contain'
+                    }}
+                    source={{
+                        uri: 'https://links.papareact.com/gzs'
+                    }}
+                />
+                <GooglePlacesAutocomplete
+                    placeholder='Where from?'
+                    nearbyPlacesAPI='GooglePlacesSearch'
+                    debounce={400}
+                    query={{
+                        key: GOOGLE_MAPS_API_KEY,
+                        language: 'en'
+                    }}
+                    fetchDetails={true}
+                    onPress={(data, details = null) => {
+                        dispatch(setOrigin({
+                            location: details?.geometry?.location,
+                            description: data?.description
+                        }))
+                        dispatch(setDestination(null));
+                    }}
+                    enablePoweredByContainer={false}
+                    minLength={2}
+                    styles={{
+                        container: {
+                            flex: 0
+                        },
+                        textInput: {
+                            fontSize: 18
+                        }
+                    }}
+                />
+                <NavOptions/>
+            </View>
         </SafeAreaView>
     )
 }
